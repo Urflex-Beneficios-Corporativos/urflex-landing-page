18492f1a26bd51d240b05831f3eb19949ae3c655
Beatriz <beaultfernandes@gmail.com>
2024-06-25 12:39:02 -0300

    landingpage

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Proposta from "./components/Propostas"
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+
+      <Home />
+     
+      <Proposta />
+
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from '../src/serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+serviceWorker.register();
