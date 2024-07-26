5d84323290897569323a6872559f9184d4ae6fd9
CristianoMends <mendescristiano012@gmail.com>
2024-07-26 10:34:29 -0300

    Refactor: substituído mapeamento de dados estáticos por JSON

--- src/components/Team/PersonCard/index.jsx
+++ src/components/Team/PersonCard/index.jsx
@@ -3,7 +3,7 @@ import { Person, Image, Title, Role, Description } from "./styles";
 
 function PersonCard({imageUrl, name, role, description }) {
     return <Person className="person-card">
-        <Image src={imageUrl}></Image>
+        <Image src={imageUrl} alt={{name}}></Image>
         <Title>{name}</Title>
         <Role>{role}</Role>
         <Description dangerouslySetInnerHTML={{ __html: description }}></Description>
